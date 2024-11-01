301d55effa1bf801372fc3109c7b0c4e6bf7529a
Omar Al Mayouf <omaralmayouf@gmail.com>
2024-11-01 04:27:44 +0300

    Deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Movie_Ratings/',
+  base: '/Movie_Ratings',
   plugins: [react()],
 })
