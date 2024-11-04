988938a0f52816bff5a1c920c48ae2e8bcb5e87f
Omar Al Mayouf <omaralmayouf@gmail.com>
2024-11-04 10:50:05 +0300

    bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useRef } from "react"
 
-const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
+const API_KEY = "e737ad74";
 
 export default function App() {
   const movieNameRef = useRef(null);
@@ -17,7 +17,7 @@ export default function App() {
 
   let getMovie = () => {
     let movieName = movieNameRef.current.value.trim();
-    let url =  `http://www.omdbapi.com/?s=${movieName}&apikey=${API_KEY}`;
+    let url =  `https://www.omdbapi.com/?s=${movieName}&apikey=${API_KEY}`;
   
     if(movieName.length <= 0) {
       // show error
@@ -34,10 +34,10 @@ export default function App() {
           let movie = data.Search[0];
           result.current.innerHTML = `<img class="object-cover object-center w-full" src="${movie.Poster}" alt="movie cover"/>`;
           movieTitleRef.current.innerHTML = `<h3>${movie.Title}</h3>`;
-          let detailUrl = `http://www.omdbapi.com/?i=${movie.imdbID}&apikey=${API_KEY}`;
+          let detailUrl = `https://www.omdbapi.com/?i=${movie.imdbID}&apikey=${API_KEY}`;
           fetch(detailUrl).then((resp) => resp.json()).then((details) => {
             movieRatingRef.current.innerHTML = `
-            <svg class="mr-3" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32"><path fill="#fcd53f" d="m18.7 4.627l2.247 4.31a2.27 2.27 0 0 0 1.686 1.189l4.746.65c2.538.35 3.522 3.479 1.645 5.219l-3.25 2.999a2.23 2.23 0 0 0-.683 2.04l.793 4.398c.441 2.45-2.108 4.36-4.345 3.24l-4.536-2.25a2.28 2.28 0 0 0-2.006 0l-4.536 2.25c-2.238 1.11-4.786-.79-4.345-3.24l.793-4.399c.14-.75-.12-1.52-.682-2.04l-3.251-2.998c-1.877-1.73-.893-4.87 1.645-5.22l4.746-.65a2.23 2.23 0 0 0 1.686-1.189l2.248-4.309c1.144-2.17 4.264-2.17 5.398 0"/></svg>
+            <svg class="mr-3" xmlns="https://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32"><path fill="#fcd53f" d="m18.7 4.627l2.247 4.31a2.27 2.27 0 0 0 1.686 1.189l4.746.65c2.538.35 3.522 3.479 1.645 5.219l-3.25 2.999a2.23 2.23 0 0 0-.683 2.04l.793 4.398c.441 2.45-2.108 4.36-4.345 3.24l-4.536-2.25a2.28 2.28 0 0 0-2.006 0l-4.536 2.25c-2.238 1.11-4.786-.79-4.345-3.24l.793-4.399c.14-.75-.12-1.52-.682-2.04l-3.251-2.998c-1.877-1.73-.893-4.87 1.645-5.22l4.746-.65a2.23 2.23 0 0 0 1.686-1.189l2.248-4.309c1.144-2.17 4.264-2.17 5.398 0"/></svg>
             <h3>${details.imdbRating}</h3>`;
             movieDetailsRef.current.innerHTML = `<h3 class="mr-3">${details.Rated}</h3> <h3 class="mr-3">${details.Year}</h3> <h3>${details.Runtime}</h3>`;
             let genre = details.Genre.split(',');
@@ -64,7 +64,7 @@ export default function App() {
         {/*input container*/}
         <div className="grid grid-cols-12 gap-5">
           {/*input field*/}
-          <input className="h-12 col-span-9 rounded-md px-6 bg-transparent border-2 border-gray-500  hover:border-gray-400 focus:outline-none focus:border-gray-300 font-medium text-lg text-white placeholder:text-gray-500 placeholder:italic "ref={movieNameRef} placeholder='Search for movies, TV shows, or animes...' type="text" name="search" id="search"></input>
+          <input className="h-12 col-span-9 rounded-md px-6 bg-transparent border-2 border-gray-500 hover:border-gray-400 focus:outline-none focus:border-gray-300 font-medium text-lg text-white placeholder:text-gray-500 placeholder:italic "ref={movieNameRef} placeholder='Search for movies, TV shows, or animes...' type="text" name="search" id="search"></input>
           {/*sumbit button*/}
           <button 
             className="h-12 col-span-3 rounded-md bg-gradient-to-b from-black to-blue-950 hover:to-blue-900 hover:scale-105 shadow-inner shadow-blue-700 text-white font-medium text-lg px-10 text-center"
